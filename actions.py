97ea74a507293854c41591dc665ab31ae37a8b50
AneeshBoreda <aneesh.boreda92@gmail.com>
2019-04-06 12:57:06 -0400

    added basic actions

--- actions.py
+++ actions.py
@@ -1,2 +1,14 @@
-def test():
-    print('hi')
+import subprocess, platform
+def open_google():
+    subprocess.call(["open","[location of google]"])
+def open_word():
+    subprocess.call(["open","[location of Word]"])
+def open_spotify():
+    subprocess.call(["open","[location of spotify]"])
+def computer_volume_0():
+    call(["amixer", "-D", "pulse", "sset", "Master", "0%"])
+def computer_volume_100():
+    call(["amixer", "-D", "pulse", "sset", "Master", "100%"])
+
+if __name__=="__main__":
+    print(platform.system())
