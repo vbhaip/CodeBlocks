8adb0dfc421ade1e9ad2045788381c8f8ac2bbc7
vbhaip <vbhaip01@gmail.com>
2019-04-06 18:20:07 -0400

    working detection of features from objects

--- templatematch.py
+++ templatematch.py
@@ -29,6 +29,8 @@ while(True):
 
 	ret, frame = cap.read()
 
+	frame = cv2.flip(frame, -1)
+
     # print(type(frame))
 
 	# frame = cv2.resize(frame, (frame.shape[1], frame.shape[0]))
@@ -37,7 +39,7 @@ while(True):
 
 	hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-	mask_hsv = cv2.inRange(hsv, low_orange_hsv, high_orange_hsv)
+	mask_hsv = cv2.inRange(hsv, low_pink_hsv, high_pink_hsv)
 	# ret,thresh = cv2.threshold(imgray,127,255,0)
 	_, contours, _ = cv2.findContours(mask_hsv,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
@@ -56,18 +58,28 @@ while(True):
 			x, y, w, h = cv2.boundingRect(c)
 
 			#gets the angle of rotation of the bounding box, if its too large, then we should give error
-			(_,_),(_,_),angle = cv2.fitEllipse(cnt)
+			(_,_),(_,_),angle = cv2.fitEllipse(c)
 
 			#gets perimeter, use this to find out length of contour
-			perimeter = cv2.arcLength(cnt,True)
+			perimeter = cv2.arcLength(c,True)
 
 			#gets number of points that are needed for for the contour
 			points = len(c)
 
-			print("x,y " + x "," + y)
-			print("angle " + angle)
-			print("perimeter " + perimeter)
-			print("points " + points)
+			epsilon = 0.01*cv2.arcLength(c,True)
+			approx = cv2.approxPolyDP(c,epsilon,True)
+
+
+
+			print("x,y ",x,",",y)
+			print("angle ", angle)
+			print("perimeter ", perimeter)
+			print("points ", len(approx))
+
+			print("\n")
+
+
+
 
 
 
@@ -78,36 +90,38 @@ while(True):
 
 	cv2.imshow('mask',mask_hsv)
 	cv2.imshow('frame', frame)
+	# while(True):
+	# 	x = 1
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	if cv2.waitKey(5000) & 0xFF == ord('q'):
 		break
 
-cap.release()
-cv2.destroyAllWindows()
-# print('done')
+# cap.release()
+# cv2.destroyAllWindows()
+# # print('done')
 
 
 
-import numpy as np
-import cv2 as cv
-cap = cv.VideoCapture('video/noChessBoard.MOV')
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-# Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-cv.imwrite('frame1.png', old_frame)
-old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-# Create a mask image for drawing purposes
-print('starting')
+# import numpy as np
+# import cv2 as cv
+# cap = cv.VideoCapture('video/noChessBoard.MOV')
+# # params for ShiTomasi corner detection
+# feature_params = dict( maxCorners = 100,
+#                        qualityLevel = 0.3,
+#                        minDistance = 7,
+#                        blockSize = 7 )
+# # Parameters for lucas kanade optical flow
+# lk_params = dict( winSize  = (15,15),
+#                   maxLevel = 2,
+#                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+# # Create some random colors
+# color = np.random.randint(0,255,(100,3))
+# # Take first frame and find corners in it
+# ret, old_frame = cap.read()
+# cv.imwrite('frame1.png', old_frame)
+# old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+# # Create a mask image for drawing purposes
+# print('starting')
 '''
 mask = np.zeros_like(old_frame)
 frame1 = old_frame
@@ -154,44 +168,44 @@ cv.destroyAllWindows()
 
 cv.waitKey(100000)
 '''
-while 1:
-    mask = np.zeros_like(old_frame)
-    ret, frame = cap.read()
-    p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-    if ret:
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+# while 1:
+#     mask = np.zeros_like(old_frame)
+#     ret, frame = cap.read()
+#     p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+#     if ret:
+#         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        blur = cv.GaussianBlur(frame, (25, 25), 0)
-        #cv.imshow('blur', blur)
-        hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
-        # HSV limits
-        lower_hsv = np.array([0, 29, 77])
-        upper_hsv = np.array([34, 74, 166])
-        mask_hsv = cv.inRange(hsv, lower_hsv, upper_hsv)
+#         blur = cv.GaussianBlur(frame, (25, 25), 0)
+#         #cv.imshow('blur', blur)
+#         hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
+#         # HSV limits
+#         lower_hsv = np.array([0, 29, 77])
+#         upper_hsv = np.array([34, 74, 166])
+#         mask_hsv = cv.inRange(hsv, lower_hsv, upper_hsv)
 
-        #approx_simple only stores points connecting the line, but approx_none saves all the points
-        contours, _ = cv.findContours(mask_hsv, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+#         #approx_simple only stores points connecting the line, but approx_none saves all the points
+#         contours, _ = cv.findContours(mask_hsv, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        for c in contours:
-            if cv.contourArea(c) > 30000:
-                M = cv.moments(c)
-                cX = int(M['m10'] / M['m00'])
-                cY = int(M['m01'] / M['m00'])
-                cv.drawContours(frame, c, -1, (255, 0, 0), 3)
-                cv.circle(frame, (cX, cY), 15, (0, 0, 255), -1)
-                cv.circle(mask_hsv, (cX, cY), 15, (0, 0, 255), -1)
+#         for c in contours:
+#             if cv.contourArea(c) > 30000:
+#                 M = cv.moments(c)
+#                 cX = int(M['m10'] / M['m00'])
+#                 cY = int(M['m01'] / M['m00'])
+#                 cv.drawContours(frame, c, -1, (255, 0, 0), 3)
+#                 cv.circle(frame, (cX, cY), 15, (0, 0, 255), -1)
+#                 cv.circle(mask_hsv, (cX, cY), 15, (0, 0, 255), -1)
 
-        img = cv.add(frame, mask)
-        img = cv.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
-        m = mask_hsv#cv.add(mask_hsv, mask)
-        m = cv.resize(m, (int(m.shape[1]/3), int(m.shape[0]/3)))
-        img = cv.flip(img, -1)
-        m = cv.flip(m, -1)
-        cv.imshow('frame',img)
-        cv.imshow('mask',m)
-        k = cv.waitKey(3000) & 0xff
-        if k == 27:
-            break
-cv.destroyAllWindows()
-cap.release()
-print('done')
+#         img = cv.add(frame, mask)
+#         img = cv.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
+#         m = mask_hsv#cv.add(mask_hsv, mask)
+#         m = cv.resize(m, (int(m.shape[1]/3), int(m.shape[0]/3)))
+#         img = cv.flip(img, -1)
+#         m = cv.flip(m, -1)
+#         cv.imshow('frame',img)
+#         cv.imshow('mask',m)
+#         k = cv.waitKey(3000) & 0xff
+#         if k == 27:
+#             break
+# cv.destroyAllWindows()
+# cap.release()
+# print('done')
