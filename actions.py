152336f53af2dad5201943ac11ee5f4e98a70e26
AneeshBoreda <aneesh.boreda92@gmail.com>
2019-04-06 13:18:15 -0400

    added more actions, like computer volume and stuff

--- actions.py
+++ actions.py
@@ -1,14 +1,30 @@
-import subprocess, platform
+import platform
+from subprocess import call
+p=platform.system()
+L="Linux"
+W="Windows"
+M="Darwin"
 def open_google():
-    subprocess.call(["open","[location of google]"])
+    call(["open","[location of google]"])
 def open_word():
-    subprocess.call(["open","[location of Word]"])
+    if p==M:
+        call(["open","[location of Word]"])
 def open_spotify():
-    subprocess.call(["open","[location of spotify]"])
-def computer_volume_0():
-    call(["amixer", "-D", "pulse", "sset", "Master", "0%"])
-def computer_volume_100():
-    call(["amixer", "-D", "pulse", "sset", "Master", "100%"])
-
+    if p==M:
+        call(["open","[location of spotify]"])
+def computer_volume(vol):
+    if p==L:
+        call(["amixer", "-D", "pulse", "sset", "Master", str(vol)+"%"])
+    elif p==M:
+        call(["osascript","-e","\"set Volume "+str(vol//10)+"\""])
+def computer_mute(bool):
+    if p==L:
+        if bool:
+            call(["amixer", "-D", "pulse", "sset", "Master", "mute"])
+        else:
+            call(["amixer", "-D", "pulse", "sset", "Master", "unmute"])
+    elif p==M:
+        call(["osascript","-e","\"set volume output muted "+bool.upper()])
 if __name__=="__main__":
     print(platform.system())
+    #"Linux","Windows","Darwin"
