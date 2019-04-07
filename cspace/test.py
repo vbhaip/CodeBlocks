943b46eb3f3053eaa3ce2d842ab5165deff6dc86
vbhaip <vbhaip01@gmail.com>
2019-04-07 02:40:54 -0400

    fix cspace

---
+++ cspace/test.py
@@ -0,0 +1,8 @@
+import cv2
+from cspaceSliders import FilterWindow
+
+cap = cv2.VideoCapture(1)
+ret, image = cap.read()
+image = cv2.resize(image, ((int)(image.shape[1]/3), (int)(image.shape[0]/3)))
+window = FilterWindow('Filter Window', image)
+window.show(verbose=True)
