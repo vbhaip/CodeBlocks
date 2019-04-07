e504427506c761a5ff2cc16c65caf33b66a2ed00
boomo <randomerz56@gmail.com>
2019-04-07 00:55:25 -0400

    added main func

--- format_obj.py
+++ format_obj.py
@@ -6,8 +6,7 @@ SEARCH_CONE_SLOPE = .15
 INDENT_DIST = 10
 
 #inp = [(50, 50, 'pink', 4), (100, 50, 'yellow', 4), (80, 100, 'pink', 4), (130, 100, 'yellow', 4), (110, 150, 'green', 4), (110, 200, 'green', 5), (80, 250, 'green', 6), (50, 300, 'green', 4)]
-#inp = [(50, 50, 'pink', 4), (100, 50, 'yellow', 4), (80, 100, 'green', 4)]
-inp = [(50, 50, 'green', 4)]
+inp = [(50, 50, 'pink', 4), (100, 50, 'yellow', 4), (80, 100, 'green', 4), (80, 150, 'green', 5)]
 
 def add_block_group(rem, last_added, last_elif, last_elif_xy):
 	cur_block = []
@@ -66,51 +65,7 @@ def add_block_group(rem, last_added, last_elif, last_elif_xy):
 blocks = add_block_group(inp, None, None, (-INDENT_DIST, 0))
 print('out', blocks)
 print('\n\n\n')
-
-def block_to_struct(blocks):
-	ind = 0
-	all_if_blocks, all_else_blocks = [], []
-	while ind < len(blocks):
-		b_group = blocks[ind]
-		if type(b_group) == type((0,0)):
-			print('x')
-			all_if_blocks.append(color_num_to_block[(b_group[2], b_group[3])])
-			ind += 1
-		else:
-			block = b_group[0]
-			# if
-			if block[2] == 'pink' and block[3] == 4:
-				print('if')
-				cond = b_group[1]
-				if_block = [block_to_struct(b_group[ind+2:])]
-				else_block = None
-				ind += 1
-				if ind < len(blocks):
-					print('else')
-					b_group = blocks[ind]
-					if type(b_group) == type((0,0)):
-						print('nvm, y')
-						print('c',cond)
-						all_if_blocks.append(ConditionStructure(color_num_to_block[(cond[2], cond[3])], [color_num_to_block[(b_group[2], b_group[3])]], None))
-						ind += 1
-					else:
-						block = b_group[0]
-						# else
-						if block[2] == 'pink' and block[3] == 5:
-							else_block = [block_to_struct(blocks[ind+1:])]
-							ind += 1
-				print(type(if_block))
-				print('c',cond)
-				all_if_blocks.append(ConditionStructure(color_num_to_block[(cond[2], cond[3])], if_block, else_block))
-			elif block[2] == 'green':
-				print('z')
-				all_if_blocks.append(color_num_to_block[(block[2], block[3])])
-				ind += 1
-	all_if_blocks.reverse()
-	all_else_blocks.reverse()
-	print(all_if_blocks, all_else_blocks)
-	return ConditionStructure(True, all_if_blocks, all_else_blocks)
-
+'''
 def blocks_to_struct(blocks):
 	ind = 0
 	all_if_blocks, all_else_blocks = [], []
@@ -151,7 +106,60 @@ def blocks_to_struct(blocks):
 				all_else_blocks.append(else_block)
 	print(all_if_blocks, all_else_blocks)
 	return ConditionStructure(True, all_if_blocks, all_else_blocks)
+'''
 
-overall_struct = block_to_struct(blocks)
+def blocks_to_struct(blocks):
+	ind = 0
+	all_if_blocks, all_else_blocks = [], []
+	print('inp', blocks)
+	while ind < len(blocks):
+		print(blocks[ind])
+		# green
+		if type(blocks[ind]) == type((0,0)):
+			print('green')
+			block = blocks[ind]
+			all_if_blocks.append(color_num_to_block[(block[2], block[3])])
+			ind += 1
+		# pink
+		else:
+			block_group = blocks[ind]
+			# if
+			print('pink',block_group)
+			if block_group[0][2] == 'pink' and block_group[0][3] == 4:
+				cond = color_num_to_block[(block_group[1][2], block_group[1][3])]
+				ind += 2
+				rest = block_group[2:]
+				all_if_blocks.append(ConditionStructure(cond, [blocks_to_struct(rest)], None))
+				'''
+				new_if_block, new_else_block = blocks_to_struct(block_group[2:])
+				new_else_block = None
+				ind += 1
+				# if then end of chain
+				if ind > len(blocks):
+					all_if_blocks.append(ConditionStructure(cond, new_if_block, None))
+					# else
+					# green
+					if type(blocks[ind]) == type((0,0)):
+						print('green')
+						block = blocks[ind]
+						all_if_blocks.append(color_num_to_block[(block[2], block[3])])
+						ind += 1
+					else:
+						block_group = blocks[ind]
+						print('bg', block_group)
+						if block_group[0][2] == 'pink' and block_group[0][3] == 5:
+							else_block = blocks_to_struct(block_group[1:])
+				all_if_blocks.append(if_block)
+				all_else_blocks.append(else_block)
+				'''
+	print(all_if_blocks, all_else_blocks)
+	return ConditionStructure(True, all_if_blocks, all_else_blocks)
+
+overall_struct = blocks_to_struct(blocks)
 print()
 print(overall_struct)
+
+def format_objects(inp):
+	blocks = add_block_group(inp, None, None, (-INDENT_DIST, 0))
+	overall_struct = blocks_to_struct(blocks)
+	
